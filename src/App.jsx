c8116cb64e3faa17efb796d81d1b15392fe2d030
Laurindo Missengo <laurindosunset@gmail.com>
2025-06-05 00:35:30 +0100

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Tasks from "./components/Tasks";
+
+function App() {
+  // State
+  return (
+    <div>
+      <h1 className="text-red-500">Gerenciador de Tarefas</h1>
+      <Tasks />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Tasks.jsx
@@ -0,0 +1,13 @@
+import AddTask from "./AddTask";
+
+function Tasks() {
+  return (
+    <div>
+      <h1>Tasks</h1>
+
+      <AddTask />
+    </div>
+  );
+}
+
+export default Tasks;
