992baa94474ff5ac1728790a2c65c4d279124ab7
Laurindo Missengo <laurindosunset@gmail.com>
2025-06-08 10:45:34 +0100

    Add the uuid library, lucide-react library and setup viewer, add  and delete buttons

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,69 @@
 import Tasks from "./components/Tasks";
 import AddTask from "./components/AddTask";
+import { useState } from "react";
+import { v4 } from "uuid";
 
 function App() {
   // State
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      title: "Estudar X",
+      description: "Estudar X é bom para resolver Y",
+      isCompleted: false,
+    },
+    {
+      id: 2,
+      title: "Estudar Y",
+      description: "Estudar X é bom para resolver Y",
+      isCompleted: false,
+    },
+    {
+      id: 3,
+      title: "Estudar Z",
+      description: "Estudar X é bom para resolver Y",
+      isCompleted: false,
+    },
+  ]);
+  function onTaskClick(taskId) {
+    const newTasks = tasks.map((task) => {
+      if (task.id === taskId)
+        return {
+          ...task,
+          isCompleted: !task.isCompleted,
+        };
+      return task;
+    });
+    setTasks(newTasks);
+  }
+  function onTaskDelete(taskId) {
+    const toDelete = tasks.filter((task) => task.id !== taskId);
+    setTasks(toDelete);
+  }
+
+  function onAddTaskSubmit(title, description) {
+    const newTask = {
+      id: v4(),
+      title,
+      description,
+      isCompleted: false,
+    };
+    setTasks([...tasks, newTask]);
+  }
+
   return (
-    <div>
-      <h1 className="text-red-500">Gerenciador de Tarefas</h1>
-      <Tasks />
-      <AddTask />
+    <div className="w-screen h-screen bg-slate-500 flex justify-center">
+      <div className="w-[500px] space-y-4">
+        <h1 className="text-3xl text-slate-100 font-bold text-center">
+          Gerenciador de Tarefas
+        </h1>
+        <AddTask onAddTaskSubmit={onAddTaskSubmit} />
+        <Tasks
+          tasks={tasks}
+          onTaskClick={onTaskClick}
+          onTaskDelete={onTaskDelete}
+        />
+      </div>
     </div>
   );
 }
--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,7 +1,31 @@
-function Tasks() {
+import { ChevronRightIcon, TrashIcon } from "lucide-react";
+
+function Tasks({ tasks, onTaskClick, onTaskDelete }) {
   return (
     <div>
-      <h1>Tasks</h1>
+      <ul className="space-y-4 p-6 bg-slate-200 rounded-md shadow">
+        {tasks.map((task) => (
+          <li key={task.id} className="flex gap-2">
+            <button
+              onClick={() => onTaskClick(task.id)}
+              className={`bg-slate-400 text-left w-full text-white p-2 rounded-md ${
+                task.isCompleted && "line-through"
+              }`}
+            >
+              {task.title}
+            </button>
+            <button className="bg-slate-400 p-2 rounded-md text-white">
+              <ChevronRightIcon />
+            </button>
+            <button
+              onClick={() => onTaskDelete(task.id)}
+              className="bg-slate-400 p-2 rounded-md text-white"
+            >
+              <TrashIcon />
+            </button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
